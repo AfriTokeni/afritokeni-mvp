b9ab9f5dca1b0ffa8843bf435a1b0248827b4589
Aigo Faith <aigofaith@gmail.com>
2025-08-12 14:08:22 +0300

    Add notification type

---
+++ src/types/notification.ts
@@ -0,0 +1,9 @@
+export interface Notification {
+  id: string;
+  title: string;
+  message: string;
+  type: 'info' | 'success' | 'warning' | 'error';
+  timestamp: Date;
+  read: boolean;
+  actionUrl?: string;
+}
