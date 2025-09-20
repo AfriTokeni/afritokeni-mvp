2cf5696c6afa8891bb94db121ca49da1a78d7468
Aigo Faith <aigofaith@gmail.com>
2025-09-20 06:50:32 +0300

    feat: add deposit-related components export in index file

---
+++ src/pages/deposit/index.ts
@@ -0,0 +1,8 @@
+// Export all deposit-related components
+export { default as AmountStep } from './AmountStep';
+export { default as AgentSelectionStep } from './AgentSelectionStep';
+export { default as ConfirmationStep } from './ConfirmationStep';
+export { default as ProgressIndicator } from './ProgressIndicator';
+
+// Export types
+export * from '../../types/depositTypes';
